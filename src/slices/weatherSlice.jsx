bb6ffaf93d17439608616f9df600e36ebcb6f962
angela <yaoliu928@gmail.com>
2023-01-31 11:33:52 +1000

    fix sth

--- src/slices/weatherSlice.jsx
+++ src/slices/weatherSlice.jsx
@@ -2,7 +2,7 @@ import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 import axios from 'axios';
 
 export const getWeather = createAsyncThunk(
-  'getWeather',
+  'weather/getWeather',
   async (city) => {
     const url = `http://api.weatherapi.com/v1/current.json?key=27e084209eca47d8ac565616232901&q=${city}&aqi=no`;
     try {
@@ -21,7 +21,7 @@ const weatherSlice = createSlice({
   extraReducers: (builder) => {
     builder
       .addCase(getWeather.fulfilled, (state, action) => {
-        state = action.payload;
+        state = action;
       })
   }
 });
