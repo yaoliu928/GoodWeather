24217ae624477aa6de3a51b1b777dabc35d86d20
angela <yaoliu928@gmail.com>
2023-02-11 17:54:45 +1000

    enable remove city

--- src/components/WeatherListItem.jsx
+++ src/components/WeatherListItem.jsx
@@ -1,9 +1,20 @@
-const WeatherListItem = ({ current, forecasts }) => {
+import { useContext } from 'react';
+
+import WeathersContext from '../context/weathersContext';
+
+const WeatherListItem = ({ id, current, forecasts }) => {
+
   const { name, temp_c, text, icon } = current;
+  const { dispatch } = useContext(WeathersContext);
 
   return (
     <li >
       <h3>{name}</h3>
+      <button onClick={() => {
+        dispatch({
+          type: 'REMOVE_CITY', payload: id
+        });
+      }}>Remove The City</button>
       <h4>Current Weather:</h4>
       <img src={icon} alt='weather icon' />
       <p>{`Temperature: ${temp_c}°C Condition: ${text}`}</p>
--- src/reducers/weathers.js
+++ src/reducers/weathers.js
@@ -2,6 +2,10 @@ const weathersReducer = (state, action) => {
   switch (action.type) {
     case 'ADD_CITY':
       return [...state, action.payload];
+    case 'REMOVE_CITY':
+      return state.filter((weather) =>
+        weather.id !== action.payload
+      )
     default:
       return state;
   }
