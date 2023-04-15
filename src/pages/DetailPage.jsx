80299b610430d984608f1e43b68c6830dcfa7713
angela <yaoliu928@gmail.com>
2023-04-15 19:36:52 +1000

    use useRef

--- src/pages/DetailPage.jsx
+++ src/pages/DetailPage.jsx
@@ -1,26 +1,24 @@
 import { Link, useParams } from "react-router-dom";
 import { useSelector, useDispatch } from 'react-redux';
+import { useEffect, useRef } from "react";
 
 import { getForecasts } from "../store/thunkAction";
-import { useEffect } from "react";
 import ForecastsList from "../components/ForecastsList";
 
 const DetailPage = () => {
-  let isInitialMount = true;
-  console.log('isInitialMount', isInitialMount);
-
+  const isInitialMount = useRef(true);
   const { id } = useParams();
   const dispatch = useDispatch();
   const cityName = useSelector((state) => state.currentWeathers)
     .find((currentWeather) => (currentWeather.id === id)).name;
-  console.log('cityname', cityName);
+
   useEffect(() => {
-    if (!isInitialMount) {
+    if (!isInitialMount.current) {
       return;
     }
     dispatch(getForecasts(cityName));
     return () => {
-      isInitialMount = false;
+      isInitialMount.current = false;
     }
   }, [cityName, dispatch]);
 
